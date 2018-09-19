51757e1cee0fafaf2c0a9d5a3b404c1bf10cc162
Marcelo Tosco @capynet <marcelo@front.id>
2018-09-19 17:52:27 +0200

    Added compile tools and sample dir.

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const browserify = require('browserify');
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const del = require("del");
+
+const pluginFile = {
+  srcFile: "jQueryDOMElementRelocation.js",
+  srcPath: "./src/",
+  dest: "./dist"
+};
+
+
+/**
+ * Clean dist dir.
+ */
+function cleanTask() {
+  return del(['dist']);
+}
+
+
+/**
+ * Converts ES6 code into compatible browser code.
+ */
+function babelizeTask() {
+  let src = pluginFile.srcPath + pluginFile.srcFile;
+
+  return browserify({entries: [src]})
+    .transform(babelify)
+    .bundle()
+    .pipe(source(pluginFile.srcFile))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(pluginFile.dest));
+
+}
+
+
+/**
+ * Watcher.
+ */
+function watchTask() {
+  gulp.watch(['src/**/*'], buildTask);
+}
+
+/**
+ * Build task.
+ */
+let buildTask = gulp.series(
+  cleanTask,
+  babelizeTask,
+);
+
+// Define tasks.
+exports.build = buildTask;
+exports.watch = watchTask;
