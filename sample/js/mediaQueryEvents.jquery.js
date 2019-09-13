e5a70b80c452b927a466585401990fc8997d9f0a
Marcelo Tosco @capynet <marcelo@front.id>
2019-09-13 20:56:05 +0200

    Updated plugin dependencies.

--- sample/js/mediaQueryEvents.jquery.js
+++ sample/js/mediaQueryEvents.jquery.js
@@ -1,58 +1,68 @@
 /* ====================================================
  * jQuery Media Query Events.
- * https://github.com/front-id/mediaQueryEvents
+ * https://github.com/frontid/mediaQueryEvents
  * Marcelo Iván Tosco (capynet)
  * ==================================================== */
 !function ($) {
   'use strict';
 
-  var plugin;
+  let singleton;
 
-  var Class = function (win, options) {
-    plugin = this;
-    this.$win = $(win);
-    var defaults = {};
-    this.options = $.extend(defaults, options);
-    init();
-    return this;
+  const MediaQueryEventsClass = function ($el, options) {
+    singleton = this;
+    this.$el = $el;
+    this.options = $.extend({}, options);
+    this.map();
   };
 
-  function init() {
-    for (var brk in plugin.options.breakpoints) {
-      // Create the matchMedia and init.
-      var mql = window.matchMedia(plugin.options.breakpoints[brk]);
+  MediaQueryEventsClass.prototype = {
 
-      // Listen.
-      var listener = function (breakpointName) {
-        return function (e) {
+    /**
+     * Map each defined breakpoint with a listener.
+     */
+    map: function () {
+      const self = this;
+
+      for (let brkNameSpace in self.options.breakpoints) {
+        // Create the matchMedia and init.
+        const mql = window.matchMedia(self.options.breakpoints[brkNameSpace]);
+
+        const listener = function (e) {
           if (e.matches) {
-            plugin.$win.trigger('mq.' + breakpointName)
+            self._fire(brkNameSpace);
           }
+        };
+
+        mql.addListener(listener);
+
+        // Since the listener will not get triggered
+        // on init we make an initial check.
+        if (mql.matches) {
+          // Wait document gets ready and all handlers are
+          // attached to the elements.
+          $(document).ready(function () {
+            self._fire(brkNameSpace);
+          });
         }
-      };
 
-      mql.addListener(listener(brk));
-
-      // Since the listener will not get triggered
-      // on init we make an initial check.
-      if (mql.matches) {
-        plugin.$win.trigger('mq.' + brk)
       }
+    },
 
-    }
+    _fire: function (brkNameSpace) {
+      const self = this;
+      self.$el.trigger('mq.' + brkNameSpace);
+    },
 
-  }
 
-  // DEFINICIÓN DEL PLUGIN
+  };
+
+
+  // Jquery plugin definition.
   //-----------------------------------------------------------
   $.fn.mediaQueryEvents = function (options) {
-    return this.each(function () {
-      var $element = $(this);
-      var data = $element.data('mediaQueryEvents');
-      if (!data) {
-        $element.data('mediaQueryEvents', (new Class(this, options)))
-      }
-    })
+    const $el = $(this);
+    const instance = new MediaQueryEventsClass($el, options);
+    $el.data('MediaQueryEventsClass', instance);
   }
 
 }(window.jQuery);
